fixed a very awful sneaky thing: samples indexs gradient should be detached

hadi/model/training.py
@@ -251,6 +251,14 @@ class OfflineTrainer:
         disc_labels, flat_indices = self.model.discriminator.get_discriminator_labels(
             to_np(x_corrupt), to_np(masked_inputs[0]), to_np(sampled_indxs[masked_labels != -100]), pretrain_mode)
 
+        # correct predictions by the generator should have lbl +1 at disc_labels
+        non_ignore_indices = torch.where(masked_labels.flatten() != -100)[0]
+        correct_predictions = torch.where(
+            masked_labels.flatten()[non_ignore_indices] == sampled_indxs.flatten()[non_ignore_indices])[0]
+        disc_zeros = torch.where(disc_labels == 0)[0]
+        needs_update_indices = disc_zeros[correct_predictions]
+        disc_labels[needs_update_indices] = 1.0
+
         disc_preds = self.model.discriminator(corrupt_hiddens, flat_indices, pretrain_mode)
         discriminator_loss = self.model.discriminator.loss_fn(disc_preds, disc_labels.to(self.device))
 
@@ -259,9 +267,8 @@ class OfflineTrainer:
             'discriminator_loss': discriminator_loss * self.train_config.loss_imbalance_lambda,
         }
 
-        num_corrects = int(torch.eq(
-            masked_labels[masked_labels != -100], sampled_indxs[masked_labels != -100]).sum().item())
-        num_total = int(torch.sum(masked_labels != -100).item())
+        num_corrects = len(correct_predictions)
+        num_total = len(non_ignore_indices)
 
         if return_extras:
             extra_outputs = {

hadi/model/transformer.py
@@ -140,7 +140,7 @@ class Generator(nn.Module):
         if self.config.generator_temperature != 1.0:
             predictions = predictions / self.config.generator_temperature
         probs = F.softmax(predictions, dim=1)
-        sampled_indxs = probs.multinomial(num_samples=1).view(labels.shape)
+        sampled_indxs = probs.multinomial(num_samples=1).view(labels.shape).detach()
 
         return predictions, sampled_indxs
 

hadi/utils/process_traj.py
@@ -235,13 +235,24 @@ def process_data(data_files, max_len=512, do_plot=True, verbose=False):
     token_types_all = segments_all.astype(int).copy()
     token_types_all[token_types_all > 0] = 1 - ((token_types_all[token_types_all > 0] % 2) - 1)
 
+    # quality control (check if each seq has at least one act and one obs token in it)
+    bad_indices = []
+    for i in range(len(sequences_all)):
+        if not (w2i['[OBS]'] in sequences_all[i] and w2i['[ACT]'] in sequences_all[i]):
+            bad_indices.append(i)
+
+    if len(bad_indices) > 0:
+        print('Found bad indices: ', bad_indices)
+
     traj_data = {
         'trajectories': trajectories, 'trajectory_token_ids': trajectory_token_ids,
         'trajectory_segment_ids': trajectory_segment_ids, 'teacher_tuples': teacher_tuples,
-        'traj_admissible_cmds_pairs': processed_traj_adm_cmds,
-        'sequence_ids': sequences_all.astype(int), 'type_ids': token_types_all.astype(int),
-        'position_ids': positions_all.astype(int), 'masks': masks_all.astype(int),
-        'segment_ids': segments_all.astype(int), 'walkthroughs_len_counts': walkthroughs_len_counts,
+        'traj_admissible_cmds_pairs': processed_traj_adm_cmds, 'walkthroughs_len_counts': walkthroughs_len_counts,
+        'sequence_ids': np.delete(sequences_all, bad_indices, axis=0).astype(int),
+        'type_ids': np.delete(sequences_all, bad_indices, axis=0).astype(int),
+        'position_ids': np.delete(positions_all, bad_indices, axis=0).astype(int),
+        'masks': np.delete(masks_all, bad_indices, axis=0).astype(int),
+        'segment_ids': np.delete(segments_all, bad_indices, axis=0).astype(int),
     }
     lang_data = {
         'verb_counts': verb_counts, 'entity_counts': entity_counts,