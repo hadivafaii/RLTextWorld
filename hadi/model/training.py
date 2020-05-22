pretty sure things are working, attention mask fixed, position_ids bug fixed

hadi/model/training.py
@@ -100,15 +100,18 @@ class OfflineTrainer:
 
         print("\nTotal Parameters:", sum([p.nelement() for p in self.model.parameters()]))
 
-    def train(self, nb_rounds, ratio_dict=None):
-        if ratio_dict is None:
-            ratio_dict = {pretrain_mode: 1 for pretrain_mode in self.pretrain_modes}
-
+    def train(self, rounds, epochs_per_round=None):
         self.model.train()
 
-        for r in range(nb_rounds):
+        if epochs_per_round is None:
+            epochs_per_round = {pretrain_mode: 1 for pretrain_mode in self.pretrain_modes}
+
+        assert type(rounds) in [int, range], "Please provide either range or int"
+
+        rounds = range(rounds) if isinstance(rounds, int) else rounds
+        for r in rounds:
             for mode in self.pretrain_modes:
-                for epoch in range(ratio_dict[mode]):
+                for epoch in range(epochs_per_round[mode]):
                     self.iteration(self.train_data, pretrain_mode=mode,
                                    r=r, epoch=epoch, train=True)
 
@@ -119,12 +122,14 @@ class OfflineTrainer:
     def valid(self):
         self.model.eval()
         with torch.no_grad():
-            self.iteration(self.valid_data)
+            for mode in self.pretrain_modes:
+                self.iteration(self.valid_data, mode)
 
     def test(self):
         self.model.eval()
         with torch.no_grad():
-            self.iteration(self.test_data)
+            for mode in self.pretrain_modes:
+                self.iteration(self.test_data, mode)
 
     def iteration(self, data_dict, pretrain_mode=None, r=0, epoch=0, train=False):
 
@@ -238,36 +243,33 @@ class OfflineTrainer:
         objects_embedded = self.model.generator.embed_objects(
             self.model.embeddings.word_embeddings, pretrain_mode, reduction='mean')
 
-        # masked_labels.transpose_(0, 1)  # max_len x batch_size
+        masked_token_ids, masked_type_ids, masked_position_ids = masked_inputs
 
         gen_preds, sampled_indxs = self.model.generator(encoder_outputs, objects_embedded, masked_labels)
         generator_loss = self.model.generator.loss_fn(gen_preds, masked_labels.flatten())
 
         x_corrupt = self.model.generator.get_x_corrupt(
-            x_masked=to_np(masked_inputs[0]),
+            x_masked=to_np(masked_token_ids),
             labels=to_np(masked_labels),
             sampled_indxs=to_np(sampled_indxs),
             pretrain_mode=pretrain_mode)
 
-        # TODO: there bunch of .T s here that won't be necessary once
-        #  functions in gen pretrain are fixed to go with (S, N) rather than (N, S)
-        corrupt_type_ids, corrupt_position_ids = compute_type_position_ids(
-            x_corrupt.T, self.model.config, starting_position_ids=to_np(masked_inputs[2].T[:, 0]))
+        # corrupt_type_ids, corrupt_position_ids = compute_type_position_ids(
+        #    x_corrupt.T, self.model.config, starting_position_ids=to_np(masked_position_ids.T[:, 0]))
 
-        corrupt_inputs = (x_corrupt, corrupt_type_ids.T, corrupt_position_ids.T)
+        corrupt_inputs = (x_corrupt, masked_type_ids, masked_position_ids)
         corrupt_inputs = tuple(
             map(
                 lambda z: torch.tensor(z, dtype=torch.long, device=self.device) if
                 type(z) is not torch.Tensor else z.to(self.device), corrupt_inputs
             )
         )
-        # corrupt_mask = self.model.create_attention_mask(corrupt_inputs[0])
 
         corrupt_hiddens, _ = self.model(src_inputs=corrupt_inputs)[0]
 
         disc_labels, flat_indices = self.model.discriminator.get_discriminator_labels(
             corrupted_token_ids=to_np(x_corrupt),
-            masked_token_ids=to_np(masked_inputs[0]),
+            masked_token_ids=to_np(masked_token_ids),
             generator_replaced_labels=to_np(sampled_indxs[masked_labels != -100]),
             gold_labels=to_np(masked_labels[masked_labels != -100]),
             pretrain_mode=pretrain_mode)

hadi/model/transformer.py
@@ -239,6 +239,9 @@ class Transformer(nn.Module):
         if config.hidden_size != config.decoder_hidden_size:
             self.decoder_src_mapping_in = nn.Linear(config.hidden_size, config.decoder_hidden_size)
 
+        # TODO: the only scenario where this is inefficient is when enc_dim == dec_dim != emb_dim
+        #  in that case there are two separate mapping in layers from emb that have identical shapes
+
         self.init_weights()
 
     def forward(self, src_inputs, tgt_inputs=None,
@@ -279,16 +282,6 @@ class Transformer(nn.Module):
         src_embedded = self.embeddings(*src_inputs)     # (S, N, E)
         if self.config.embedding_size != self.config.hidden_size:
             src_embedded = self.encoder_embedding_mapping_in(src_embedded)      # (S, N, H_enc)
-
-        # create attention masks if not provided
-        # if src_mask is None:
-        #     src_mask = self.create_attention_mask(src_inputs[2] > 0)
-        # TODO: verify this below is actualy what memory_mask is meant to do:
-        #  I just found out that no this is not correct because: attn_mask: (N*num_heads, T, S)
-        #  So maybe memory_mask = src_mask[:, :max_len_decoder, :],
-        #  where max_len_decoder = len(tgt_inputs[0])
-        # if memory_mask is None:
-        #    memory_mask = src_mask
 
         if src_key_padding_mask is None:
             src_key_padding_mask = src_inputs[0].T == self.config.pad_id

hadi/utils/gen_pretrain_data.py
@@ -241,6 +241,7 @@ def _get_masked_input(x, ranges, unk_id=3):
 
 
 def compute_type_position_ids(x, config, starting_position_ids=None):
+    # TODO: this function is absolutely fucking not necessary
     if starting_position_ids is None:
         starting_position_ids = np.one(len(x))
 
@@ -250,6 +251,9 @@ def compute_type_position_ids(x, config, starting_position_ids=None):
             (0, x.shape[-1] - tup[1]), constant_values=config.pad_id), axis=0)
             for tup in zip(starting_position_ids, (x > 0).sum(1))]
     )
+
+    # The [TRAJ] always has position = 1
+    # position_ids[:, 0] = np.ones(len(x))
 
     type_ids = (np.ones(x.shape, dtype=int) * config.pad_id).flatten()
 
@@ -316,7 +320,7 @@ def generate_corrupted_data(inputs, config, conversion_dict=None, bag_of_object_
     masked_token_ids = masked_token_ids[~np.all(masked_token_ids == config.pad_id, axis=1)]
     labels_arr = labels_arr[~np.all(labels_arr == -100, axis=1)]
     masked_type_ids, masked_position_ids = compute_type_position_ids(masked_token_ids, config, starting_pos_ids)
-    return [masked_token_ids, masked_type_ids, masked_position_ids], labels_arr.astype(int)
+    return [masked_token_ids, masked_type_ids, position_ids], labels_arr.astype(int)
 
 
 # ------------------------------------------------------------------------------------------------------- #